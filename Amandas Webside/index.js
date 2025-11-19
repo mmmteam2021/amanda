f2c647c1afe58f30b2e6cea83079c73ae4804791
mmmteam2021 <mmmteam2021@gmail.com>
2025-11-19 11:12:24 +0100

    Delete Amandas Webside/index.js

--- Amandas Webside/index.js
+++
@@ -1,9 +0,0 @@
-let question = document.getElementsByClassName("q")
-console.log(question[0])
-
-
-question[0].addEventListener("click", function () {
-    let answer = document.getElementsByClassName("an")
-    console.log(answer[0])
-    answer[0].classList.toggle("hide")
-});
